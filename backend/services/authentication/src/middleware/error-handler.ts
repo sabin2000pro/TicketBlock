f41774df5ec77eb83451c196b311b3108f9005e1
sabin2000pro <sabinlungu293@gmail.com>
2022-10-20 15:02:16 +0100

    Implementing code for the authentication error handling

--- backend/services/authentication/src/middleware/error-handler.ts
+++ backend/services/authentication/src/middleware/error-handler.ts
@@ -37,3 +37,25 @@ export const errorHandler = (err: Error, request: Request, response: Response, n
     return next();
 }
 
+
+export class BadRequestError extends CustomError {
+    statusCode = StatusCodes.BAD_REQUEST;
+    status = "Bad Request Error."
+
+  constructor(message: any, statusCode: any) {
+        super(message);
+        this.statusCode = statusCode;
+  }
+
+}
+
+export class NotFoundError extends CustomError {
+    statusCode = StatusCodes.BAD_REQUEST;
+    status = "Resource not found on the server. Check resource again"
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+  }
+
+}
