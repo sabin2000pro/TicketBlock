6c74fd4429139692e29a878b58b5e734fea317db
sabin2000pro <sabinlungu293@gmail.com>
2022-10-24 10:35:08 +0100

    Implementing the NFT model / data seeder to load the data in the database

--- frontend/contracts/Auction.sol
+++ frontend/contracts/Auction.sol
@@ -6,7 +6,8 @@ import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract Auction {
-    
+    uint256 immutable startingPrice = 0.030 ether;
+    uint256 immutable reductionPrice = 0.010 ether;
 
     constructor() {
 
