0968f3e36b87475603dcf869303aef39804889b3
sabin2000pro <sabinlungu293@gmail.com>
2022-10-24 12:35:41 +0100

    Adding configuration env variables

--- backend/services/authentication/src/database/auth-schema.ts
+++ backend/services/authentication/src/database/auth-schema.ts
@@ -2,17 +2,18 @@ import mongoose from "mongoose";
 require('dotenv').config();
 
 export default () => {
+    
     const connectAuthSchema = async () => {
         
         return await mongoose.connect(process.env.DB_URI!).then(connection => {
 
 
             if(connection.connection) {
-                return console.log(`Connected to AUTH database..`)
+                return console.log(`Connected to auth schema..`)
             }
 
             else {
-                return console.log(`Could not connect to auth database`)
+                return console.log(`Could not connect to the auth schema`)
             }
 
 
