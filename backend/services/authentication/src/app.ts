20631c3ae1e878959cd906bc48a0cb017d0bbdd6
sabin2000pro <sabinlungu293@gmail.com>
2022-10-24 12:33:47 +0100

    Adding configuration env variables

--- backend/services/authentication/src/app.ts
+++ backend/services/authentication/src/app.ts
@@ -6,6 +6,7 @@ import hpp from "hpp"
 import helmet from "helmet"
 import mongoSanitize from "express-mongo-sanitize";
 import cors from "cors";
+import { errorHandler } from "./middleware/error-handler";
 
 import authRouter from './routes/auth-routes';
 import connectAuthSchema from './database/auth-schema';
@@ -17,7 +18,6 @@ const app: Application = express();
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-//test
 
 if(process.env.NODE_ENV === 'production') {
     app.use(mongoSanitize()); // Prevent against NoSQL Injection attacks in production environment
@@ -34,5 +34,6 @@ app.use(cookieSession({
 
 // Error Handler middleware
 app.use('/api/v1/auth', authRouter);
+app.use(errorHandler);
 
 export {app}
