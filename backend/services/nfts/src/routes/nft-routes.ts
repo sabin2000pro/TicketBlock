bb5d0a7ca70594b6e6c6c0f88581c58b8ac73cbb
sabin2000pro <sabinlungu293@gmail.com>
2022-10-31 08:56:06 +0000

    Implementing NFt routes

--- backend/services/nfts/src/routes/nft-routes.ts
+++ backend/services/nfts/src/routes/nft-routes.ts
@@ -1,3 +1,7 @@
 import express, { Router } from "express";
+import { fetchAllNfts, fetchNftByID } from "../controllers/nft-controller";
 
 export const nftRoutes: Router = express.Router({mergeParams: true});
+
+nftRoutes.route("/").get(fetchAllNfts)
+nftRoutes.route('/:id').get(fetchNftByID);
