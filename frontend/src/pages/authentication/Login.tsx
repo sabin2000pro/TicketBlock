7d08f1021e1940cadc43ed1f583b252ec677b5b9
sabin2000pro <sabinlungu293@gmail.com>
2022-10-31 13:24:25 +0000

    Implementing login functionality for frontend

--- frontend/src/pages/authentication/Login.tsx
+++ frontend/src/pages/authentication/Login.tsx
@@ -1,13 +1,25 @@
 import { Button } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState<string | undefined>("")
 
   const handleLogin = async () => {
+    try {
+
+    } 
+    
+    catch(error: any) {
+
+      if(error) {
+        return console.error(error);
+      }
+
+
+    }
+
     
   }
-
-
 
   return (
     <>
@@ -25,7 +37,7 @@ const Login = () => {
             <div className = "email-container">
 
               <label htmlFor= "username">E-mail</label>
-                <input type = "text" placeholder='E-mail Address'/>
+                <input value = {email} onChange = {(event) => setEmail(event.target.value)} type = "text" placeholder='E-mail Address'/>
 
             </div>
 
