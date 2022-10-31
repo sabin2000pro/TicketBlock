d3b60480dca4a1917ea2b32a90cc268568fdd602
sabin2000pro <sabinlungu293@gmail.com>
2022-10-31 14:39:56 +0000

    Fixing logout bug

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -113,7 +113,10 @@ const NavBar: React.FC = () => {
                <a href = "/nfts"> <li className = "link">Ticket Block</li></a>
 
                <a href = "/register"> <li className = "link">Register</li></a>
-    {tokenPresent ? <a onClick = {logoutHandler} href = "/login"> <li className = "link"> Logout </li> </a> : <a href = "/login"> <li className = "link"> Login </li> </a>  }
+    {tokenPresent && isLoggedIn ? <a onClick = {logoutHandler} href = "/login"> <li className = "link"> Logout </li> </a> : 
+      
+      !tokenPresent && !isLoggedIn &&
+    <a href = "/login"> <li className = "link"> Login </li> </a>  }
                <a href = "/create-nft"> <li className = "link">Create NFT</li></a>
                <a href = "/profile"> <li className = "link">My Profile</li></a>
                <a href = "/cart"> <li className = "link">Cart</li></a>
