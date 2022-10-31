0f6bc7881ded14c481defafc29d264227106a950
sabin2000pro <sabinlungu293@gmail.com>
2022-10-31 20:43:32 +0000

    Implemented get logged in user

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -1,21 +1,24 @@
 import React, {useEffect, useState} from 'react'
-import axios from 'axios';
 import { getLoggedInUser } from '../api/auth-api';
 
 const UserProfile = () => {
 
-    const [user, setUser] = useState([])
-    
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
 
         const fetchUserData = async () => {
-            getLoggedInUser();
+
+            const response = await getLoggedInUser();
+            setUser(response.data.username);
+
+            console.log(user);
+            
         }
 
         fetchUserData();
 
-    }, [])
+    }, [user])
 
 
   return (
@@ -24,11 +27,13 @@ const UserProfile = () => {
 
         <div className = "profile-container">
 
-            <h1 className = "heading-primary">Logged In As: </h1>
+            <h1 className = "heading-primary">You are logged in as : {user}</h1>
+
+            
 
             <div className = "user-profile">
                
-               
+               <h2>Account Balance : </h2>
             </div>
 
 
