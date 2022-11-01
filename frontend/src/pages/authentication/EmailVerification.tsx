3f35652665a9388a48f27d7024e9e6ef82f6cfd7
sabin2000pro <sabinlungu293@gmail.com>
2022-11-01 09:11:12 +0000

    Adding API to verify e-mail address

--- frontend/src/pages/authentication/EmailVerification.tsx
+++ frontend/src/pages/authentication/EmailVerification.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import { Button } from "@chakra-ui/react";
 
 const EmailVerification: React.FC = () => {
 
@@ -6,6 +7,7 @@ const EmailVerification: React.FC = () => {
   const [otpSubmitted, setOtpSubmitted] = useState<boolean | undefined>(false)
 
   const handleOtpSubmission = async () => {
+
       try {
 
       } 
@@ -18,7 +20,25 @@ const EmailVerification: React.FC = () => {
   return (
 
     <>
+  <div className = "forgot-container">
 
+<div className = "forgot-form">
+
+ <form onSubmit = {handleOtpSubmission} method = "POST">
+
+ <h1 className = "heading-primary">Forgot Password</h1>
+
+     <div className = "email-container">
+
+     </div>
+
+     <Button type = "submit" className = "submit-btn" colorScheme='teal' size ='md'>Submit</Button>
+
+ </form>
+
+
+  </div>
+  </div>
     </>
 
 
