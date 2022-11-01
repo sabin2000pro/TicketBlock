b15d26167c9cede29d71959c7e9a10b6dba270e6
sabin2000pro <sabinlungu293@gmail.com>
2022-11-01 19:56:40 +0000

    Verifying CRUD functionality for backend for the nfts

--- frontend/src/api/nft-api.ts
+++ frontend/src/api/nft-api.ts
@@ -1,28 +1,78 @@
 
 export const fetchAllNfts = async () => {
+    try {
 
-}
+    } 
+    
+    catch(error: any) {
+
+    }
+} 
 
 export const fetchNftByID = async (id: number) => {
+    try {
 
+    } 
+    
+    catch(error: any) {
+
+    }
 }
 
 export const createNft = async () => {
+    try {
+
+    } 
     
+    catch(error: any) {
+
+    }
+
 }
 
 export const editNft = async (id: number) => {
 
+    try {
+
+    } 
+    
+    catch(error: any) {
+
+    }
+
 }
 
 export const deleteNftByID = async (id: number) => {
+    try {
+
+    } 
+    
+    catch(error: any) {
+
+    }
 
 }
 
 export const deleteAllNfts = async () => {
 
+    try {
+
+    } 
+    
+    catch(error: any) {
+
+    }
+
 }
 
 export const uploadNftImage = async () => {
+
+    try {
+
+    } 
+    
+    catch(error: any) {
+
+    }
     
 }
