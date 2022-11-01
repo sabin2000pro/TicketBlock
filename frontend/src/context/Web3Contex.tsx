a799546c5e7f412e13fb266797e0dd900c4f7910
sabin2000pro <sabinlungu293@gmail.com>
2022-11-01 14:57:38 +0000

    Working on adding Global web3 state

--- frontend/src/context/Web3Contex.tsx
+++ frontend/src/context/Web3Contex.tsx
@@ -1,30 +1,81 @@
 import { MetaMaskInpageProvider } from '@metamask/providers';
 import React, {useContext, useState, useEffect, createContext, ReactNode} from 'react';
+import Web3, { providers } from "ethers";
+import { ethers } from 'ethersv5';
 
 type Web3ContextProps = {
     children: ReactNode
 }
 
-type IWeb3Context = {
-    
-    handleFetchAccount: () => void
-    handleSwitchAccounts: () => void
+export const defaultState = {
+    ethereum: null,
+    provider: null,
+    isLoading: false
 }
 
-export const Web3Context = createContext({} as IWeb3Context)
+type Web3State = {
+    web3Api: any
+    getAccount: () => void
+    getAccountBalance: () => void
+
+
+}
+
+type EthProvider = {
+    ethereum: MetaMaskInpageProvider | null
+    provider: providers.Web3Provider | null
+}
+
+export const createDefaultState = () => {
+    return {ethereum: null, provider: null}
+}
+
+export const initialiseWeb3State = ({ethereum, provider}: any) => {
+    return {ethereum, provider}
+}
+
+export const Web3Context = createContext<Web3State>(createDefaultState() as any)
 
 export const Web3Provider = ({children}: Web3ContextProps) => {
-    const [ethereum, setEthereum] = useState(null);
 
-    const handleFetchAccount = () => {
+    const [web3Api, setWeb3Api] = useState(defaultState)
+    const [accounts, setAccounts] = useState("")
+    const [balance, setBalance] = useState(0);
 
-    }
+   useEffect(() => {
 
-    const handleSwitchAccounts = () => {
+      try {
 
-    }
+        const initialiseWeb3 = async () => {
+          
+            const globalEth = window.ethereum;
+            const ethProvider = new ethers.providers.Web3Provider(globalEth as any)
+            
+            setWeb3Api(initialiseWeb3State({ethereum: globalEth, provider: ethProvider, isLoading: true}) as any)
+           
+          }
+
+          initialiseWeb3();
+    
+      } 
+      
+      
+      catch(error: any) {
+       
+      }
+    
+     
+   }, [])
+
+   const getAccountBalance = () => {
+    
+   }   
+
+   const getAccount = () => {
+
+   }
  
-    return <Web3Context.Provider value = {{handleFetchAccount, handleSwitchAccounts}}>
+    return <Web3Context.Provider value = {{web3Api, getAccount, getAccountBalance}}>
             {children}
     </Web3Context.Provider>
 }
