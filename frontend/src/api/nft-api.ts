02fe28bdfaa1a3354b2198a5582d08014711a2c7
sabin2000pro <sabinlungu293@gmail.com>
2022-11-01 19:55:22 +0000

    Fixing a couple of issues relatewd to logout

--- frontend/src/api/nft-api.ts
+++ frontend/src/api/nft-api.ts
@@ -1,9 +1,28 @@
 
+export const fetchAllNfts = async () => {
+
+}
+
+export const fetchNftByID = async (id: number) => {
+
+}
+
 export const createNft = async () => {
     
 }
 
-export const editNft = async () => {
+export const editNft = async (id: number) => {
 
 }
 
+export const deleteNftByID = async (id: number) => {
+
+}
+
+export const deleteAllNfts = async () => {
+
+}
+
+export const uploadNftImage = async () => {
+    
+}
