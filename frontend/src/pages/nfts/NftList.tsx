b5a211e530be30e4ddddc0287f34538e110100f3
sabin2000pro <sabinlungu293@gmail.com>
2022-11-02 08:50:58 +0000

    Added logo to the navigation bar

--- frontend/src/pages/nfts/NftList.tsx
+++ frontend/src/pages/nfts/NftList.tsx
@@ -1,19 +1,22 @@
 import { Grid, GridItem, Box, Badge } from '@chakra-ui/react'
 import React, {useState, useEffect} from 'react'
 
+type INftVals = {
 
-const NftList: React.FC = (props: any) => {
+}
+
+const NftList: React.FC<INftVals> = (props: any) => {
 
     return (
 
       <>
-      
+        <h1>All NFTs here</h1>
       </>
 
       
     )
 
-    }
+  }
 
 
 export default NftList
