b07ea049d53be7fbadb2d7df78f648c83eac1aba
sabin2000pro <sabinlungu293@gmail.com>
2022-11-02 12:20:29 +0000

    Adding code to verify login MFA

--- frontend/src/components/CodeInput.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CodeInput = () => {
-  return (
-    <div>CodeInput</div>
-  )
-}
-
-export default CodeInput
--- frontend/src/pages/authentication/LoginMfaVerification.tsx
+++ frontend/src/pages/authentication/LoginMfaVerification.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Button } from "@chakra-ui/react";
 
 const LoginMfaVerification = () => {
+  const [mfaCode, setMfaCode] = useState("");
+
+  const handleMfaVerification = async () => {
+
+  }
+
+
+
   return (
-    <div>LoginMfaVerification</div>
+    <>
+    
+
+    <div className = "forgot-container">
+
+<div className = "forgot-form">
+
+<form onSubmit = {handleMfaVerification} method = "POST">
+
+<h1 className = "heading-primary">Login Verification</h1>
+
+      <div className = "pin-container">
+
+      <input value = {mfaCode} onChange = {(event) => setMfaCode(event.target.value)} type = "text" placeholder='Enter OTP'/>
+
+      </div>
+
+    <Button type = "submit" className = "submit-btn verify-btn" colorScheme='teal' size ='lg'>Verify Account</Button>
+
+</form>
+
+
+</div>
+
+</div>
+
+    
+    </>
   )
 }
 
