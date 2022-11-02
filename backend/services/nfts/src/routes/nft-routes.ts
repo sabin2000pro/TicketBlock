1387cdf6d696f21978b4b2ff4202a0c77c954b8a
sabin2000pro <sabinlungu293@gmail.com>
2022-11-02 09:36:24 +0000

    Adding an avatar gto user profile

--- backend/services/nfts/src/routes/nft-routes.ts
+++ backend/services/nfts/src/routes/nft-routes.ts
@@ -1,7 +1,8 @@
+import { verifyUserAuth } from './../../../authentication/src/middleware/verify-auth';
 import express, { Router } from "express";
 import { fetchAllNfts, fetchNftByID, createNewNft, deleteAllNfts, deleteNftByID } from "../controllers/nft-controller";
 
 export const nftRoutes: Router = express.Router({mergeParams: true});
 
-nftRoutes.route("/").get(fetchAllNfts).post(createNewNft).delete(deleteAllNfts)
+nftRoutes.route("/").get(verifyUserAuth as any, fetchAllNfts).post(verifyUserAuth as any, createNewNft).delete(verifyUserAuth as any, deleteAllNfts)
 nftRoutes.route('/:id').get(fetchNftByID).delete(deleteNftByID)
