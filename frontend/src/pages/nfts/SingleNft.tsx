1f36ddda56fdf5e70c912b5fddf2079e437881c0
sabin2000pro <sabinlungu293@gmail.com>
2022-11-02 09:16:16 +0000

    Adding edit nfts and fetch NFT by id in the App.tsx file

--- frontend/src/pages/nfts/SingleNft.tsx
+++ frontend/src/pages/nfts/SingleNft.tsx
@@ -5,7 +5,11 @@ const SingleNft: React.FC = () => {
   return (
 
     <>
-
+      <div className = "nft-container">
+         <h1 className = "heading-primary">
+            Single NFT Here
+         </h1>
+      </div>
     </>
 
 
