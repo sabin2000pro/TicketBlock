b711e2125adbe19d9f0854a2ebb3e65b7f46a8f3
sabin2000pro <sabinlungu293@gmail.com>
2022-11-02 09:58:51 +0000

    Writing API to update profile settings

--- backend/services/authentication/src/controllers/auth-controller.ts
+++ backend/services/authentication/src/controllers/auth-controller.ts
@@ -271,7 +271,7 @@ export const updateProfileDetails = async(request: IGetUserData, response: Respo
 
     console.log(user);
 
-    if(fieldsToUpdate.email || fieldsToUpdate.username || fieldsToUpdate.password) {
+    if(fieldsToUpdate.email || fieldsToUpdate.username) {
         return next(new BadRequestError("Missing fields, please check again", 400));
     }
 
