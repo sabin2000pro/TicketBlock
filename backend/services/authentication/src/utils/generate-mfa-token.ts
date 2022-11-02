bc1ec024642f2b83cd968d77b9dba7f50581d4b0
sabin2000pro <sabinlungu293@gmail.com>
2022-11-02 12:29:43 +0000

    Adding code to verify login MFA

---
+++ backend/services/authentication/src/utils/generate-mfa-token.ts
@@ -0,0 +1,14 @@
+
+export const generateMfaToken = (token_length = 6): any => {
+
+    let theMfaToken = ''
+    let RANDOM_LENGTH = 9;
+
+    for(let i = 1; i <= token_length; i++) {
+
+       const randomMfaToken = Math.round(Math.random() * RANDOM_LENGTH)
+       theMfaToken += randomMfaToken;
+    }
+
+    return theMfaToken;
+}
