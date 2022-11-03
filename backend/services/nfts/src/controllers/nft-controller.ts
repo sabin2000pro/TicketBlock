40934f04287216037f0220b25313cf4b974cfcf0
sabin2000pro <sabinlungu293@gmail.com>
2022-11-03 10:44:51 +0000

    Working on code to place an NFT on sale

--- backend/services/nfts/src/controllers/nft-controller.ts
+++ backend/services/nfts/src/controllers/nft-controller.ts
@@ -31,6 +31,9 @@ export const fetchNftByID = async (request: Request, response: Response, next: N
 export const createNewNft = async (request: Request, response: Response, next: NextFunction): Promise<Response | any> => {
     const body = request.body;
     const nft = await Nft.create(body);
+    let newNftId = nft.id++;
+
+    console.log(`New NFT ID : `, newNftId);
 
     if(nft) {
         return next(new BadRequestError("NFT Already created on the backend", StatusCodes.BAD_REQUEST));
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useContext } from 'react'
 import { Web3Context } from '../context/Web3Context'
 
